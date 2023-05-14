e15338805f608fbff5e37539ed385fb1872f601e
Christian Tröhler <christian.troehler@students.fhnw.ch>
2023-05-14 16:48:13 +0200

    add CRUD endpoints for personas

--- PromptEditor/Web.hs
+++ PromptEditor/Web.hs
@@ -3,21 +3,23 @@
 module PromptEditor.Web where
 
 import PromptEditor.Types
-import qualified PromptEditor.DB as DB
-import           Database.SQLite.Simple (withConnection, Connection)
-import           Control.Applicative
-import           Control.Monad (mzero)
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Monoid (mconcat)
-import qualified Data.Text.Lazy as T
-import           Data.Aeson hiding (json)
-import           Web.Scotty.Trans
-import           Control.Monad.Reader
+import PromptEditor.Application
+import Database.SQLite.Simple (withConnection, Connection)
+import Control.Applicative ()
+import Control.Monad (mzero)
+import Control.Monad.IO.Class (liftIO)
+import Data.Monoid (mconcat)
+import Data.Aeson ( eitherDecode )
+import Web.Scotty.Trans
+import Control.Monad.Reader ( ReaderT(runReaderT), MonadTrans (lift) )
+import Network.Wai.Middleware.RequestLogger ( logStdoutDev )
+import Network.Wai.Middleware.Static ( (>->), addBase, noDots, staticPolicy )
+import System.Exit ( exitFailure )
+import System.IO ( hPutStrLn, stderr )
+
 import qualified Data.ByteString.Lazy as BL
-import           Network.Wai.Middleware.RequestLogger
-import           Network.Wai.Middleware.Static
-import           System.Exit
-import           System.IO
+import qualified Data.Text.Lazy as T
+import qualified PromptEditor.DB as DB
 
 main :: IO ()
 main = do
@@ -46,3 +48,26 @@ application = do
     middleware $ staticPolicy (noDots >-> addBase "web/build")
 
     get "/" $ file "web/build/index.html"
+
+    get "/personas" $ do
+        personas <- lift getAllPersonasAction
+        json personas
+    
+    get "/personas/:id" $ do
+        id <- param "id"
+        persona <- lift $ getPersonaAction id
+        json persona
+    
+    post "/personas/" $ do
+        data' <- jsonData
+        persona <- lift $ createPersonaAction data'
+        json (personaId persona)
+    
+    put "/personas/:id" $ do
+        id <- param "id"
+        data' <- jsonData
+        lift $ updatePersonaAction id data'
+    
+    delete "/personas/:id" $ do
+        id <- param "id"
+        lift $ deletePersonaAction id
